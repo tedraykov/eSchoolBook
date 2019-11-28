ce11b4b29e276d6a3c0337ecda51430470adf4fa
Simona <simonaeneva@yahoo.com>
2019-11-28 21:07:03 +0200

    Implemented UserService and JWT authorization

--- server/BusinessLogicLayer/Services/BaseService.cs
+++ server/BusinessLogicLayer/Services/BaseService.cs
@@ -1,21 +1,25 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using SchoolBook.DataAccessLayer.Entities;
+using SchoolBook.DataAccessLayer.Interfaces;
 
 namespace SchoolBook.BusinessLogicLayer.Services
 {
     public class BaseService
     {
-        protected BaseService(
+        protected IRepositories Repositories;
+        protected UserManager<User> UserManager { get; set; }
+        protected IMapper Mapper { get; set; }
+        
+        public BaseService(
+            IRepositories repositories,
             UserManager<User> userManager,
             IMapper mapper)
         {
+            this.Repositories = repositories;
             this.UserManager = userManager;
             this.Mapper = mapper;
         }
 
-        protected UserManager<User> UserManager { get; set; }
-
-        protected IMapper Mapper { get; set; }
     }
 }
