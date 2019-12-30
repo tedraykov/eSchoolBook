d0e2d42dc70e27a0f697d70e26016445f44a82f3
Simona <simonaeneva@yahoo.com>
2019-12-30 21:12:47 +0200

    Updated StudentsToClass and Absence table

--- server/DataAccessLayer/Entities/StudentToGrade.cs
+++ server/DataAccessLayer/Entities/StudentToGrade.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using SchoolBook.DataAccessLayer.Entities.SchoolUserEntities;
 
 namespace SchoolBook.DataAccessLayer.Entities
 {
     public class StudentToGrade
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        
         public string StudentId { get; set; }
 
         public Student Student { get; set; }
@@ -18,5 +22,7 @@ namespace SchoolBook.DataAccessLayer.Entities
         public Subject Subject { get; set; }
 
         public DateTime DateCreated { get; set; }
+        
+        public DateTime DateModified { get; set; }
     }
 }
