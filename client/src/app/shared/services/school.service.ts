f95a8b6066dd87e329e7aa29a0a33e55bc3bc6b3
Teodor Raykov <tedraykov@gmail.com>
2020-01-15 17:22:50 +0200

    Add School

--- client/src/app/shared/services/school.service.ts
+++ client/src/app/shared/services/school.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {School} from "../models/school.interface";
+import {SchoolInputModel} from "../../admin-ui/add-school/models/school-input.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
   readonly url = 'http://localhost:5000';
-  private readonly endpoint = '/school';
+  private readonly schoolEndpoint = 'school';
   constructor(private http: HttpClient) { }
 
   getAllSchools$(): Observable<School[]> {
-    return this.http.get<School[]>(this.url + this.endpoint);
+    return this.http.get<School[]>(`${this.url}/${this.schoolEndpoint}`);
+  }
+
+  addSchool$(school: SchoolInputModel): Observable<void> {
+    return this.http.post<void>(`${this.url}/${this.schoolEndpoint}`, school);
   }
 }
